2679eb5bbbd7add4000ec647a8ffc07d886f0045
Jessybr <jessicabuenoramos5@gmail.com>
2024-04-22 21:18:13 -0300

    feat: adicioando funcionalidade no btn-show-more

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+
+  showText: boolean = false;
+  btnShow: boolean = true;
+  point: boolean = true;
+  btnShowLess: boolean = false;
+
+  showMore(): void {
+    this.showText = true;
+    this.btnShow = false;
+    this.point = false;
+    this.btnShowLess = true;
+  }
+
+  showLess(): void {
+    this.showText = false;
+    this.btnShowLess = false;
+    this.btnShow = true;
+    this.point = true;
+  }
 }
